5d443f27692706825f775947cfe6ebd3839e95b3
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-06-26 16:35:49 +0200

    buttons styled with tailwindcss

--- src/elements/Button.vue
+++ src/elements/Button.vue
@@ -2,7 +2,9 @@
   <button
     @click="handleClick"
     :class="[
-      modeSelected ? 'elpriser-tab chosen w-button' : 'elpriser-tab w-button',
+      modeSelected
+        ? 'min-w-36 sm:min-w-48 bg-transparent border-4 border-dashed border-[#57a3af] rounded-xl m-2 p-2.5 font-straw_bold text-lg transition-transform duration-200 text-white bg-[#f29091] border-transparent'
+        : 'min-w-36 sm:min-w-48 bg-transparent border-4 border-dashed border-[#57a3af] rounded-xl m-2 p-2.5 font-straw_bold text-lg transition-transform duration-200',
     ]"
   >
     {{ text }}
