4c28541e6bf3fd53ab0b6d07cfe9e8104be52a3c
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-06-26 14:10:21 +0200

    start tailwind implementation

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        straw_light: ["Strawford Light Webfont", "sans-serif"],
+        straw_bold: ["Strawford Bold Webfont", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        thin: "3rem 1fr 3rem",
+        slim: "0.25fr 1fr 0.25fr",
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: true,
+  },
+};
