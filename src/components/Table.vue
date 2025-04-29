796e15ca4adcb15cc85ea024d931c7a6cefddd1d
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-04-29 22:21:49 +0200

    added span prop and dynamic table generation

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -79,37 +79,10 @@ export default {
       type: String,
       required: true,
     },
-  },
-
-  data() {
-    return {
-      tableLabels: [
-        "00 - 01",
-        "01 - 02",
-        "02 - 03",
-        "03 - 04",
-        "04 - 05",
-        "05 - 06",
-        "06 - 07",
-        "07 - 08",
-        "08 - 09",
-        "09 - 10",
-        "10 - 11",
-        "11 - 12",
-        "12 - 13",
-        "13 - 14",
-        "14 - 15",
-        "15 - 16",
-        "16 - 17",
-        "17 - 18",
-        "18 - 19",
-        "19 - 20",
-        "20 - 21",
-        "21 - 22",
-        "22 - 23",
-        "23 - 00",
-      ],
-    };
+    span: {
+      type: String,
+      required: true,
+    },
   },
 
   computed: {
@@ -138,6 +111,26 @@ export default {
     formattedAveragePrice() {
       const price = this.currentPrices.averagePrice;
       return price ? (parseFloat(price) / 10).toFixed(2).toString() : "";
+    },
+
+    tableLabels() {
+      const labels = [];
+      let from = 0;
+      let to = 1;
+
+      if (this.span === "1") {
+        for (let i = 0; i < this.currentPrices?.prices?.length; i++) {
+          labels.push(
+            from.toString().padStart(2, "0") +
+              " - " +
+              to.toString().padStart(2, "0"),
+          );
+          from++;
+          to++;
+        }
+      }
+
+      return labels;
     },
   },
 
